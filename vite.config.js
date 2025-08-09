8a64f11669fea667ed5a0fe48dcd5b58d2f5ac85
Student-Lokeshgarhwal <lokeshgarhwal1805@gmail.com>
2025-08-09 14:27:23 +0530

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/AI-chatbot/",
-  plugins: [react()],
+  base: '/AI-chatbot/',   // ← matches your repo name
+  plugins: [react()]
 })
